316484f6719847fdaa277708112b06f5ac4ee735
Nick Srivastava <nicksriv@outlook.com>
2025-11-10 05:51:14 -0800

    Fix missing Image import in security-scanner page

--- app/security-scanner/page.tsx
+++ app/security-scanner/page.tsx
@@ -2,6 +2,7 @@
 
 import { motion } from 'framer-motion';
 import Link from 'next/link';
+import Image from 'next/image';
 import { CodeDiffView, AllChecksPassed } from '../../components/GitHubElements';
 
 export default function SecurityScanner() {
